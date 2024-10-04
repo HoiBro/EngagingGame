211eee74285e277c400eeff34763279a74b3777b
HoiBro1 <finndegroot2008@gmail.com>
2024-10-04 12:17:05 +0200

    Basic platforming properties.

--- godotgame/scripts/player.gd
+++ godotgame/scripts/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 1000
+@export var JUMP_VELOCITY = -400.0
+@export var AIR_FRICTION = 2000
+@export var GROUND_FRICTION = 5000
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +12,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = velocity.x + direction * delta * SPEED
+	elif is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, GROUND_FRICTION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, AIR_FRICTION * delta)
 
 	move_and_slide()
