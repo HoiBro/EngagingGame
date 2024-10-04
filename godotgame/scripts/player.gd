0df981b758144cc1bdff95f52e09a0f70e1e88ec
peppy8 <pepijn.brokken@gmail.com>
2024-10-04 17:25:14 +0200

    yipee de code werkt niet mee :)

--- godotgame/scripts/player.gd
+++ godotgame/scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 1000
-@export var jump_velocity = -400.0
+@export var jump_velocity = -750.0
 @export var air_friction = 2000
 @export var ground_friction = 5000
 
@@ -17,19 +17,19 @@ func _physics_process(delta: float) -> void:
 	
 	# Get the input direction and handle the movement/deceleration.
 	var DIRECTION = Input.get_axis("move_left", "move_right")
-	
-	if DIRECTION:
-		if is_on_floor():
-			if((velocity.x > 0) != (DIRECTION > 0) and velocity.x != 0):
-				velocity.x = move_toward(velocity.x, 0, (ground_friction + DIRECTION * speed) * delta)
-			else:
-				velocity.x = move_toward(velocity.x, DIRECTION * speed, ground_friction * delta)
-		else:
-			velocity.x = move_toward(velocity.x, DIRECTION * speed, air_friction * delta)
-	else:
-		if is_on_floor():
-			velocity.x = move_toward(velocity.x, 0, ground_friction * delta)
-		else:
-			velocity.x = move_toward(velocity.x, 0, air_friction * delta)
 
+	handle_friction(DIRECTION,delta)
+	handle_movement(DIRECTION,delta)
+	
 	move_and_slide()
+
+func handle_movement(DIR,delta):
+	if DIR: 
+		velocity.x = DIR * speed
+
+func handle_friction(DIR,delta):
+	if is_on_floor():
+		move_toward(velocity.x, 0, ground_friction * delta)
+		print(ground_friction * delta)
+	else:
+		move_toward(velocity.x, 0, air_friction * delta)
