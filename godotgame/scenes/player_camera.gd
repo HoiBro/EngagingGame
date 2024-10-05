cd9306636a5807ce6aededb87c08cac353efeb23
peppy8 <pepijn.brokken@gmail.com>
2024-10-05 16:27:26 +0200

    tile terrain werkt nu

--- godotgame/scenes/player_camera.gd
+++ godotgame/scenes/player_camera.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.get_axis("zoom_in","zoom_out") != 0:
 		ZOOM_EXPONENT += Input.get_axis("zoom_in","zoom_out")
 		zoom.x = pow(10, ZOOM_EXPONENT)
--- godotgame/sprite_2d.gd
+++ godotgame/sprite_2d.gd
@@ -6,6 +6,6 @@ func _ready() -> void:
 	set_flip_v(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
 	set_flip_v(cos(rotation) < 0)
