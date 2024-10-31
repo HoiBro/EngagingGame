81aac4381ed29319f7f98905e9969635c0c5c425
peppy8 <pepijn.brokken@gmail.com>
2024-10-31 10:26:32 +0100

    classes dingen

--- godotgame/scenes/weapon.gd
+++ godotgame/scenes/weapon.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
-class Weapon:
-	var Equip_speed
-	var Max_range
-	var equiped: bool
+class_name Weapon
+
+var Equip_speed: float
+var Unequip_speed: float
+
+class Projectile_weapon:
+	var projectile_speed: float
+	var projectile_gravity: float
+	var Max_range: float
+
+class Explosive:
+	var Explosion_radius: float
+	var Explosion_damage: float
