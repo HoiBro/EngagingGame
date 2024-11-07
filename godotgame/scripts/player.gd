00609ad420efa1a4bf3d942a010d5234e1d577d5
HoiBro1 <finndegroot2008@gmail.com>
2024-11-07 10:27:12 +0100

    Fixed coyote time and buffer time

--- godotgame/scripts/player.gd
+++ godotgame/scripts/player.gd
@@ -6,18 +6,16 @@ extends CharacterBody2D
 @export var jump_velocity = 750
 @export var air_friction = 200
 @export var ground_friction = 3000
-@export var jump_buffer_time = 0.07
+@export var jump_buffer_time = 0.03
 @export var bunnyhop_speed = 50
-@export var coyote_time = 0.5
-
-@onready var coyote_timer = $coyoteTimer
-@onready var buffer_timer = $bufferTimer
-
+@export var coyote_time = 0.1
 
 var DIRECTION: float
 var POS_DELTA_MOUSE: Vector2
-var CAN_JUMP: bool = false
-var JUMP_BUFFER: bool = false
+var HAS_JUMPED: bool = false
+var BUFFER_TIMER_START: bool = false
+var COYOTE_TIMER: float = 0
+var BUFFER_TIMER: float = 0
 
 func _ready() -> void:
 	$Shotgun.connect("shotgun_fired",handle_recoil_shotgun)
@@ -32,19 +30,27 @@ func _physics_process(delta: float) -> void:
 	
 	if !is_on_floor():
 		velocity += get_gravity() * delta
-		if CAN_JUMP:
-			if coyote_timer.is_stopped():
-				coyote_timer.start(coyote_time)
+		if BUFFER_TIMER_START:
+			BUFFER_TIMER += delta
+		if BUFFER_TIMER > jump_buffer_time:
+			BUFFER_TIMER = 0
+			BUFFER_TIMER_START = false
+		if !HAS_JUMPED:
+			COYOTE_TIMER += delta
+			if COYOTE_TIMER > coyote_time:
+				COYOTE_TIMER = 0
+				HAS_JUMPED = true
 		else:
 			pass
 			#var distance = position - TileMapLayer.position #distance to tilemap is hard to get
 			#if distance.abs < 30:
 				#print(distance) #put walljump code here
 	else:
-		CAN_JUMP = true
-		if JUMP_BUFFER:
+		HAS_JUMPED = false
+		COYOTE_TIMER = 0
+		if BUFFER_TIMER != 0 && BUFFER_TIMER <= jump_buffer_time:
 			handle_jump(DIRECTION)
-			JUMP_BUFFER = false
+			BUFFER_TIMER = 0
 	
 	handle_friction(delta)
 	handle_movement(DIRECTION,delta)
@@ -55,10 +61,12 @@ func handle_jump(DIR):
 	if is_on_floor():
 		velocity.y -= jump_velocity
 		velocity.x += DIR * bunnyhop_speed
-		CAN_JUMP = false
+		HAS_JUMPED = true
 	else:
-		JUMP_BUFFER = true
-		buffer_timer.start(jump_buffer_time)
+		BUFFER_TIMER_START = true
+		if COYOTE_TIMER != 0 && COYOTE_TIMER <= coyote_time:
+			velocity.y -= jump_velocity
+			velocity.x += DIR * bunnyhop_speed
 
 func handle_movement(DIR,delta):
 	if DIR:
@@ -77,12 +85,6 @@ func handle_friction(delta):
 func handle_recoil_shotgun(POS,RECOIL):
 	var FORCE = -POS.normalized() * RECOIL
 	velocity += FORCE
-
-func on_jump_buffer_timeout():
-	JUMP_BUFFER = false
-
-func on_coyote_timeout():
-	CAN_JUMP = false
 
 func handle_forces():
 	var forces = null
