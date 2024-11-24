7da1fc4af71fa2c7ec76257675c96b62c83b951c
HoiBro <finndegroot2008@gmail.com>
2024-11-25 00:47:53 +0100

    I despise raycasts

--- godotgame/scripts/enemy.gd
+++ godotgame/scripts/enemy.gd
@@ -3,12 +3,14 @@ extends RigidBody2D
 var enemy_stats = {
 	"health": 100
 }
+@onready var shotgun = $"../Player/Shotgun"
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"fire shotgun"):
-		await $"../Player/Shotgun".SPACE_STATE.collider
-		if $"../Player/Shotgun".SPACE_STATE.collider == RigidBody2D:
+		await shotgun.raycast_result
+		if shotgun.result.collider == RigidBody2D:
+			print("FUCK ME")
 			enemy_stats.health -= 10
-			if enemy_stats.health <= 0:
-				get_tree().queue_delete($".")
-	print(enemy_stats.health)
+		if enemy_stats.health <= 0:
+			get_tree().queue_delete($".")
+		print(enemy_stats.health)
--- godotgame/scripts/shotgun.gd
+++ godotgame/scripts/shotgun.gd
@@ -3,14 +3,14 @@ extends Node2D
 @export var reload_time = 1.8
 @export var recoil = 1000
 @export var ready_to_fire: bool = true
-@export var raycast = Vector2(50, 100)
+@export var raycast = 100
+@export var result = {}
 
 @onready var player: CharacterBody2D = $".."
+@onready var shotgun_model: Sprite2D = $"../PlayerSprite/Sprite2D/Shotgun"
 
-var SPACE_STATE = {
-	"position": Vector2(0, 0),
-	"collider": Object
-}
+var MPOS: Vector2 = Vector2(0, 0)
+signal raycast_result
 
 func _ready() -> void:
 	position = Vector2(25, 10)
@@ -18,10 +18,20 @@ func _ready() -> void:
 
 func _input(event) -> void:
 	if event.is_action_pressed(&"fire shotgun") && ready_to_fire:
-		player.velocity += -get_local_mouse_position().normalized() * recoil
+		MPOS = -get_local_mouse_position().normalized()
+		player.velocity += MPOS * recoil
+		
 		get_tree().create_timer(reload_time, false).timeout.connect(_on_reload_timer_timeout)
+		
 		$AudioStreamPlayer.play()
-		SPACE_STATE = get_world_2d().direct_space_state.intersect_ray(PhysicsRayQueryParameters2D.create(-get_local_mouse_position().normalized()*(Vector2(0, 0), raycast)))
+		
+		var query = PhysicsRayQueryParameters2D.create(shotgun_model.position + player.position, shotgun_model.position + player.position + MPOS * raycast, 4294967295, [player])
+		var cast = get_world_2d().direct_space_state.intersect_ray(query)
+		if cast:
+			print(cast)
+			result.collider = cast.collider
+			raycast_result.emit()
+		
 		ready_to_fire = false
 
 func _on_reload_timer_timeout() -> void:
