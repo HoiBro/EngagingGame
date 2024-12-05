f8b381dc7cbc60124bdd521448dc8e30a26fc1e3
Brokken <815012@leerling.eckartcollege.nl>
2024-12-05 11:09:05 +0100

    added grappling hook on right mouse button also Q and E can be used for shotgun and grappling hook respectively

--- godotgame/scenes/grappling_hook.gd
+++ godotgame/scenes/grappling_hook.gd
@@ -1,33 +1,31 @@
 extends Node2D
 
 @export var reload_time = 1.8
-@export var recoil = 1000
+@export var pull = 1000
 @export var ready_to_fire: bool = true
-@export var raycast_length = 100
+@export var raycast_length = 1000
 @export var result: Dictionary = {} # global dictionary for raycasting
 
 @onready var player: CharacterBody2D = $".."
-@onready var shotgun_model: Sprite2D = $"../PlayerSprite/Sprite2D/ShotgunSprite"
+@onready var grappling_hook_model: Sprite2D = $"../PlayerSprite/Sprite2D/GrapplingHookSprite"
 
 var MPOS: Vector2 = Vector2(0, 0)
-var SHOTGUN_POSITION: Vector2 = Vector2(0, 0)
+var GRAPPLING_HOOK_POSITION: Vector2 = Vector2(0, 0)
 var QUERY: PhysicsRayQueryParameters2D
 var CAST: Dictionary = {}
 signal raycast_result
 
 func _input(event) -> void:
-	if event.is_action_pressed(&"fire shotgun") && ready_to_fire:
+	if event.is_action_pressed(&"fire grappling hook") && ready_to_fire:
 		MPOS = get_local_mouse_position().normalized()
-		SHOTGUN_POSITION = shotgun_model.position + player.position
-		player.velocity -= MPOS * recoil
+		GRAPPLING_HOOK_POSITION = grappling_hook_model.position + player.position
+		player.velocity += MPOS * pull
 		player.just_jumped = false
 		
 		get_tree().create_timer(reload_time, false).timeout.connect(_on_reload_timer_timeout)
 		
-		$AudioStreamPlayer.play()
-		
 		result = {}
-		QUERY = PhysicsRayQueryParameters2D.create(SHOTGUN_POSITION, SHOTGUN_POSITION + MPOS * raycast_length, 1, [player])
+		QUERY = PhysicsRayQueryParameters2D.create(GRAPPLING_HOOK_POSITION, GRAPPLING_HOOK_POSITION + MPOS * raycast_length, 1, [player])
 		CAST = get_world_2d().direct_space_state.intersect_ray(QUERY)
 		for i in CAST:
 			result[i] = CAST[i] # update global dictionary
